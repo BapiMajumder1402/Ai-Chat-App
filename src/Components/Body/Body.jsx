cf7e80573f4ede9d9f8e4b6fbf14328c09728fe7
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-05-20 20:17:07 +0530

    components added

---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+
+function Body() {
+  return (
+    <div>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>
+  )
+}
+
+export default Body
